9f9feb98adbec2a101127a3463da36980f0eb05c
manuelandersen <manuelandersen9@gmail.com>
2024-06-27 20:18:42 -0400

    added attributes to playerspider

--- padelscraper/padelscraper/spiders/players.py
+++ padelscraper/padelscraper/spiders/players.py
@@ -46,6 +46,61 @@ class PlayerSpider(scrapy.Spider):
         attributes["name"] = response.xpath('//h2[@class="slider__name player__name"]/text()').get()
         attributes["country"] = response.xpath('//p[@class="slider__country player__country"]/text()').get()
         attributes["points"] = response.xpath('//span[@class="slider__pointTNumber player__pointTNumber"]/text()').get()
+        attributes["paired_with"] = response.xpath('//span[@class="content"]/a/text()').get()
+        attributes["position"] = response.xpath('//div[@class="additionalInfo__hand"]/span[@class="content"]/text()').get()
+        attributes["birth_date"] = response.xpath('//div[@class="additionalInfo__birth"]/span[@class="additionalInfo__data"]/text()').get()
+        attributes["height"] = response.xpath('//div[@class="additionalInfo__height"]/span[@class="additionalInfo__data"]/text()').get()
+        attributes["place_of_birth"] = response.xpath('//div[@class="additionalInfo__born"]/span[@class="additionalInfo__data"]/text()').get().strip()
+
+        residence = response.xpath('//div[@class="additionalInfo__lives"]/span[@class="additionalInfo__data"]/text()').get()
+        if residence == "--":
+            attributes["residence"] = None
+        else:
+            attributes["residence"] = residence
+
+        matches_played = response.xpath('//div[@class="matchPlayer__played"]/span/text()').get()
+        if matches_played == "--":
+            matches_played = None
+        else:
+            attributes["matches_played"] = matches_played
+
+
+        matches_won = response.xpath('//div[@class="matchPlayer__won"]/span/text()').get()
+        if matches_won == "--":
+            matches_won = None
+        else:
+            attributes["matches_won"] = matches_won
+
+        matches_lost = response.xpath('//div[@class="lost"]/span/text()').get()
+        if matches_lost == "--":
+            matches_lost = None
+        else:
+            attributes["matches_lost"] = matches_lost
+
+        consecutive_victories = response.xpath('//div[@class="matchPlayer__victories"]/span/text()').get()
+        if consecutive_victories == "--":
+            consecutive_victories = None
+        else:
+            attributes["consecutive_victories"] = consecutive_victories
+
+        effectiveness = response.xpath('//div[@class="matchPlayer__effective"]/span/text()').get()
+        if effectiveness == "--":
+            effectiveness = None
+        else:
+            attributes["effectiveness"] = effectiveness
+        
+        old_position = None
+        # Check for oldPosition down
+        number_down = response.xpath('//span[contains(@class, "oldPosition down")]/text()').get()
+        if number_down:
+            old_position = f"-{number_down}"
+        # Check for oldPosition up if position is not set
+        elif old_position is None:
+            number_up = response.xpath('//span[contains(@class, "oldPosition up")]/text()').get()
+            if number_up:
+                old_position = f"+{number_up}"
+
+        attributes["old_position"] = old_position
 
         yield {
             **attributes
